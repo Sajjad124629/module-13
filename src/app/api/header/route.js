6ec1c36d6755b95f0b6d368b72e2d34593b1329f
Sajjad124629 <sh8170468@gmail.com>
2023-08-29 23:22:10 +0600

    Date:29-8-2023 Time:11:22PM

--- src/app/api/header/route.js
+++ src/app/api/header/route.js
@@ -5,5 +5,8 @@ export async function GET(request, response) {
     const list = headers();
     const user_id = list.get('user_id');
     const user_email = list.get('user_email');
-    return NextResponse.json({user_id:user_id, user_email:user_email});
+    // return NextResponse.json({user_id:user_id, user_email:user_email});
+    return NextResponse.json([
+        {user_id:user_id, user_email:user_email}
+      ],{status:201,headers:{'user_id':user_id,"user_email":user_email}});
    }
